2c4229bdb2398f8a132fdac84761c04e8db67ec4
Abrar Ur Alam <safee20142@gmail.com>
2025-12-10 14:23:36 +0600

    Add JS to hamburger icon to toggle animations

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 function borgir(){
     let hamburger_button = document.getElementById('boigah');
-    
+    if(hamburger_button.classList.contains('open')){
+        hamburger_button.classList.replace('open', 'close');
+    }else{
+        hamburger_button.classList.replace('close', 'open');
+    }
 }
