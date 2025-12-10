276df070dd72003e8e22bb9b774f5e41de2fe119
Abrar Ur Alam <safee20142@gmail.com>
2025-12-10 17:02:20 +0600

    Sync minor keyframing mismatch

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 function borgir(){
     let hamburger_button = document.getElementById('boigah');
+    let hamburger_menu = document.getElementById('hamburger-menu');
     if(hamburger_button.classList.contains('open')){
         hamburger_button.classList.replace('open', 'close');
+        hamburger_menu.style.left = '-200px';
     }else{
         hamburger_button.classList.replace('close', 'open');
+        hamburger_menu.style.left = '0px';
     }
 }
